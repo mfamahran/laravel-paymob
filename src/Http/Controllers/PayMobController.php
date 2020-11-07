97dd9d76ed7fc4bf9483680fd4be0a15027ef2a3
Mostafa Mahran <mfamahran@gmail.com>
2020-11-07 11:42:43 +0200

    Update PayMobController.php

--- src/Http/Controllers/PayMobController.php
+++ src/Http/Controllers/PayMobController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use mfamahran\PayMob\Facades\PayMob;
+use Mfamahran\PayMob\Facades\PayMob;
 
 class PayMobController extends Controller
 {
